f8a7b69d3776d88c48e65c390bfc5e6e12a7c5b1
ValadeAurelien <valade.aurelien69580@gmail.com>
2018-01-19 11:36:29 +0100

    ven. janv. 19 11:36:29 CET 2018

--- PoissonSeq/src/parameters.hxx
+++ PoissonSeq/src/parameters.hxx
@@ -9,50 +9,50 @@
 class Parameters : public GetPot {
 public:
 
-  Parameters(int argc, char **argv);
-  ~Parameters();
-  std::ostream & out() { return *m_out; }
-  void info();
+    Parameters(int argc, char **argv);
+    ~Parameters();
+    std::ostream & out() { return *m_out; }
+    void info();
 
-  int n(int i) const { return m_n[i]; }
-  double dx(int i) const { return m_dx[i]; }
+    int n(int i) const { return m_n[i]; }
+    double dx(int i) const { return m_dx[i]; }
 
-  int imin(int i) const { return m_imin[i]; }
-  int imax(int i) const { return m_imax[i]; }
-  int di(int i) const { return m_di[i]; }
+    int imin(int i) const { return m_imin[i]; }
+    int imax(int i) const { return m_imax[i]; }
+    int di(int i) const { return m_di[i]; }
   
-  int itmax() const { return m_itmax; }
-  double dt() const { return m_dt; }
+    int itmax() const { return m_itmax; }
+    double dt() const { return m_dt; }
 
-  int output() const { return m_output; }
-  std::string resultPath() const { return m_path; }
-  bool help();
+    int output() const { return m_output; }
+    std::string resultPath() const { return m_path; }
+    bool help();
 
-  bool convection() const { return m_convection; }
-  bool diffusion() const { return m_diffusion; }
+    bool convection() const { return m_convection; }
+    bool diffusion() const { return m_diffusion; }
 
-  void convection(bool b) { m_convection = b; }
-  void diffusion(bool b) { m_diffusion = b; }
+    void convection(bool b) { m_convection = b; }
+    void diffusion(bool b) { m_diffusion = b; }
 
-  int nthreads() const { return m_nthreads; }
-  void nthreads(int n) { m_nthreads = n; }
+    int nthreads() const { return m_nthreads; }
+    void nthreads(int n) { m_nthreads = n; }
   
 private:
-  std::ostream * m_out;
+    std::ostream * m_out;
 
-  int m_nthreads;
-  int m_n[3];
-  double m_dx[3];
-  int m_imin[3], m_imax[3], m_di[3];
+    int m_nthreads;
+    int m_n[3];
+    double m_dx[3];
+    int m_imin[3], m_imax[3], m_di[3];
   
-  int m_itmax;
-  double m_dt;
-  bool m_convection, m_diffusion;
+    int m_itmax;
+    double m_dt;
+    bool m_convection, m_diffusion;
   
-  int m_output;
+    int m_output;
 
-  std::string m_path;
-  bool m_help;
+    std::string m_path;
+    bool m_help;
 
 };
 
